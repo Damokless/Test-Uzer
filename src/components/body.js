76dc2b4adbdc4c9e49077946238a39c54a2a8013
Damokles <damokless@protonmail.com>
2021-02-05 01:55:22 +0100

    Last push

--- src/components/body.js
+++ src/components/body.js
@@ -4,10 +4,9 @@ import "../assets/styles/Components.css";
 
 function Body() {
   let [x, setX] = useState(0);
-  let [y, setY] = useState();
-  let dataX = parseFloat(x)
-  let dataY = parseFloat(y)
-  console.log(typeof(x))
+  let [y, setY] = useState(0);
+  let dataX = parseFloat(x);
+  let dataY = parseFloat(y);
   let buttonstyle = "";
   let buttonoption = "";
   let styledisable = "cursor-not-allowed buttonHeavy50 w-36 h-10 rounded";
@@ -22,6 +21,7 @@ function Body() {
     buttonstyle = styleenable;
     buttonoption = buttonenable;
   }
+
   return (
     <>
       <div className="m-5">
@@ -33,8 +33,6 @@ function Body() {
           <div className="p-8">
             <input
               className="inputroman text-black w-36 h-10 rounded border-transparent p-5 m-2"
-              id="xInput"
-              name="x"
               type="number"
               min="0"
               placeholder="X"
@@ -42,40 +40,45 @@ function Body() {
             ></input>
             <input
               className="inputroman text-black w-36 h-10 ring-0 rounded p-5 m-2"
-              id="yInput"
-              name="y"
               type="number"
               min="0"
               placeholder="Y"
               onChange={({ target: { value } }) => setY(value)}
             ></input>
-            <button
-              className={buttonstyle}
-              type="submit"
-              id="submitButton"
-              disabled={buttonoption}
-            >
-              Visualiser
-            </button>
           </div>
         </form>
-        <Chart
-          width={"250px"}
-          height={"250px"}
-          chartType="PieChart"
-          loader={<div>Loading Chart</div>}
-          data={[["Value", "Numbers"], ["X", dataX], ["Y", dataY] ]}
-          options={{
-            legend: "none",
-            // Just add this option
-            pieHole: 0.5,
-            slices: {
-              0: { color: "#FFC953" },
-              1: { color: "grey" },
-            },
-          }}
-          rootProps={{ "data-testid": "3" }}
-        />
+        <div className="p-10">
+          <button
+            className={buttonstyle}
+            type="submit"
+            disabled={buttonoption}
+          >
+            Visualiser
+          </button>
+        </div>
+        <div className="mx-16">
+          <Chart
+            width={"250px"}
+            height={"250px"}
+            chartType="PieChart"
+            loader={<div>Loading Chart</div>}
+            data={[
+              ["Value", "Numbers"],
+              ["X", dataX],
+              ["Y", dataY],
+            ]}
+            options={{
+              legend: "none",
+              // Just add this option
+              pieHole: 0.5,
+              slices: {
+                0: { color: "#FFC953" },
+                1: { color: "grey" },
+              },
+            }}
+            rootProps={{ "data-testid": "3" }}
+          />
+        </div>
       </div>
     </>
   );
